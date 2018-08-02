25014cdb1e7898267a5df20449543dc632ca3c94
local resource <155235319@qq.com>
2018-08-02 14:53:27 +0800

    python练习

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
 import os
-print "Hello Python!"
+
+print ('Hello Python')
+
+True or False
+
+True and False
+
+'''list是有序集合'''
+classmates=['Kate','Jack','Mickle']
+print(classmates)
+len(classmates)
+
+'''索引下标-1表示取最后一个元素，以此类推，-2倒数第二，-3倒数第三'''
+classmates[-1]
+
+'''元组'''
+classmates1=('a','b','c')
+classmates1
+
+'''练习‘’‘
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+L[0][0]
+L[1][1]
+L[2][2]
