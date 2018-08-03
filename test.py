dd3a530bff319478300edc74698982ba3a966134
local resource <155235319@qq.com>
2018-08-03 09:37:26 +0800

    学习记录

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-import numpy
+import math
 
 print ('Hello Python')
 
@@ -48,7 +48,7 @@ else :
 
 height=200
 high=1.8
-BMI=height/(numpy.square(high)) '''平方函数'''
+BMI=height/(manth.pow(high)) '''平方函数'''
 
 print(BMI)
 if BMI<18.5:
@@ -93,3 +93,23 @@ print(c['a'])
 
 d = set((1,2,3))
 print(b)
+
+'''
+定义函数时，需要确定函数名和参数个数；
+如果有必要，可以先对参数的数据类型做检查；
+函数体内部可以用return随时返回函数结果；
+函数执行完毕也没有return语句时，自动return None。
+函数可以同时返回多个值，但其实就是一个tuple。
+***定义默认参数要牢记一点：默认参数必须指向不变对象！
+参数：必选参数、默认参数、可变参数、关键字参数和命名关键字参数
+'''
+def add(numbers):
+    sum = 0
+    n = 1
+    for number in range(numbers):
+        sum = sum + number
+        if(n <= number):
+            n = n +1
+    return sum
+
+add(10)
