8986fbc39e557367a4a2904b0b35a182dc493848
local resource <155235319@qq.com>
2018-08-03 11:27:28 +0800

    函数学习记录

--- func.py
+++ func.py
@@ -4,7 +4,7 @@
 函数体内部可以用return随时返回函数结果；
 函数执行完毕也没有return语句时，自动return None。
 函数可以同时返回多个值，但其实就是一个tuple。
-***定义默认参数要牢记一点：默认参数必须指向不变对象！
+***定义默认参数要牢记一点：默认参数必须指向不变对象！如果是可变对象，程序运行时会有逻辑错误
 参数：必选参数、默认参数、可变参数、关键字参数和命名关键字参数
 '''
 
@@ -56,4 +56,25 @@ person('zhangsheng',35,city='shanghai',job='tester')
 '''如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了'''
 def pepeole(name,age,*args,city,job):
     print(name,age,args,city,job)
-pepeole('qingtian',25,)
+pepeole('qingtian',25,[1,2,3],city='shanghai',job='engeene')
+
+'''参数组合
+参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+'''
+
+
+'''对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的
+虽然可以组合多达5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差。
+'''
+def product(x,y):
+    return x * y
+
+product(4,5)
+
+def producttest(*args):
+    cal = 1
+    for n in args:
+        cal = cal * n
+    return cal
+
+producttest(5,6,7,9)
