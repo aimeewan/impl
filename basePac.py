8d2b9f9965891a163376b05782948ea23805e6b6
local resource <155235319@qq.com>
2018-08-08 17:20:31 +0800

    练习题

---
+++ basePac.py
@@ -0,0 +1,14 @@
+
+mate = [2,4,'n',25,'hi',8]
+mate.sort()
+print(mate)
+
+mate1 = [2,4,34,25,12,8]
+mate1.sort()
+print(mate1)
+
+wan = [
+    w1('Dave','A'),
+    w2('Jane','C'),
+    w3('Wanch','B')
+]
